9f6e355f4c9292bf23ac6d68e1dbf8c53c19a849
alan <ala.bravo@duocuc.cl>
2025-05-22 22:17:23 -0400

    Arreglos y completado del proyecto Evaluaciones

---
+++ src/main/java/com/edutech/evaluaciones/controller/EvaluacionController.java
@@ -0,0 +1,27 @@
+package com.edutech.evaluaciones.controller;
+
+import com.edutech.evaluaciones.model.Evaluacion;
+import com.edutech.evaluaciones.service.EvaluacionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/evaluaciones")
+@CrossOrigin(origins = "*")
+public class EvaluacionController {
+
+    @Autowired
+    private EvaluacionService evaluacionService;
+
+    @GetMapping
+    public List<Evaluacion> listarEvaluaciones() {
+        return evaluacionService.obtenerTodas();
+    }
+
+    @PostMapping
+    public Evaluacion crearEvaluacion(@RequestBody Evaluacion evaluacion) {
+        return evaluacionService.guardarEvaluacion(evaluacion);
+    }
+}
